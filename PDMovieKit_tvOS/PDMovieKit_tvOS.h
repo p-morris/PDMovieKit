139a54f25306ced306857791b256faf9d678f330
p-morris <pete@iosfaststart.com>
2019-03-29 13:57:28 +0000

    Added build target for iOS and tvOS

---
+++ PDMovieKit_tvOS/PDMovieKit_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  PDMovieKit_tvOS.h
+//  PDMovieKit_tvOS
+//
+//  Created by Peter Morris on 29/03/2019.
+//  Copyright © 2019 Pete Morris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDMovieKit_tvOS.
+FOUNDATION_EXPORT double PDMovieKit_tvOSVersionNumber;
+
+//! Project version string for PDMovieKit_tvOS.
+FOUNDATION_EXPORT const unsigned char PDMovieKit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDMovieKit_tvOS/PublicHeader.h>
+
+
